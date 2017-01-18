949163997b228f57d667ed81c90d1b74bd8801ab
Joe Stump <joe@sprint.ly>
2017-01-18 11:14:55 -0800

    Add a test cookbook.

---
+++ test/cookbooks/git2consul-test/metadata.rb
@@ -0,0 +1,9 @@
+name             'git2consul-test'
+maintainer       'Sprint.ly, Inc.'
+maintainer_email 'YOUR_OPSCODE_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures git2consul-test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'git2consul'
---
+++ test/cookbooks/git2consul-test/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: git2consul-test
+# Recipe:: default
+#
+# Copyright 2017, Sprint.ly, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
